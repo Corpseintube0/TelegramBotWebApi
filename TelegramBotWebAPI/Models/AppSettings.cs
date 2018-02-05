83624901699e2f391d63b47f2e678da62632a657
R. Flarsen <infrared990@gmail.com>
2018-02-05 15:45:39 +0700

    Update AppSettings.cs

--- TelegramBotWebAPI/Models/AppSettings.cs
+++ TelegramBotWebAPI/Models/AppSettings.cs
@@ -4,8 +4,8 @@
     {
         public static string Url { get; set; } = ""; //TODO: добавить адрес веб-сервера
 
-        public static string Name { get; set; } = "newRfTestBot"; //имя бота
+        public static string Name { get; set; } = ""; //имя бота
 
-        public static string Key { get; set; } = "469228799:AAFSc4IQ6765ss9BpyeGI5V8biYg6o2UTpY"; //токен бота
+        public static string Key { get; set; } = ""; //токен бота
     }
 }
